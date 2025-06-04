049b74a6f9b1d0944f29effa253124c2896d9948
DeepestWind <1041031300@qq.com>
2025-06-04 17:02:41 +0800

    area-tree-init

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import AreaSelect from "@/components/AreaSelect/index.vue";
+
 defineOptions({
   name: "Welcome"
 });
 </script>
 
 <template>
-  <h1>Pure-Admin-Thin（非国际化版本）</h1>
+  <div class="app-container">
+      <AreaSelect />
+  </div>
 </template>
+
+<style scoped>
+.app-container {
+  padding: 0;
+  display: flex;
+  justify-content: flex-start;
+}
+
+:deep(.el-tree) {
+  max-width: 300px !important;
+  width: 300px;
+  border-right: 1px solid #e4e7ed;
+  padding: 10px;
+  height: calc(100vh - 80px);
+  overflow-y: auto;
+}
+</style>
